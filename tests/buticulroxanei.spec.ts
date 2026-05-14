daff1eed11fc574ef909479dc3c357637d0dc818
Stefan Ciobotaru <stefancristianciobotaru@gmail.com>
2026-05-14 08:37:14 +0300

    import helpers

--- tests/buticulroxanei.spec.ts
+++ tests/buticulroxanei.spec.ts
@@ -1,26 +1,26 @@
 import { expect, type Page, test } from '@playwright/test';
-import { assertTitle, assertVisible, click, fill, findByCss, getText, pressKey } from './helpers';
+import * as helpers from './helpers';
 
 async function searchCatalog(page: Page, query: string): Promise<void> {
-  const searchInput = findByCss(page, 'input.ui-autocomplete-input').first();
-  await assertVisible(searchInput);
-  await fill(searchInput, query);
-  await pressKey(searchInput, 'Enter');
+  const searchInput = helpers.findByCss(page, 'input.ui-autocomplete-input').first();
+  await helpers.assertVisible(searchInput);
+  await helpers.fill(searchInput, query);
+  await helpers.pressKey(searchInput, 'Enter');
 }
 
 test('get first product price', async ({ page }) => {
   await page.goto('/');
 
-  await assertTitle(page, /Buticul Roxanei/);
-  await assertVisible(findByCss(page, 'input.ui-autocomplete-input').first());
+  await helpers.assertTitle(page, /Buticul Roxanei/);
+  await helpers.assertVisible(helpers.findByCss(page, 'input.ui-autocomplete-input').first());
 
   await searchCatalog(page, 'figurina');
 
   const productName = 'Margaretă';
-  const product = findByCss(page, 'article.product-miniature').filter({ hasText: productName });
-  await click(product.locator('a.thumbnail.product-thumbnail').first());
+  const product = helpers.findByCss(page, 'article.product-miniature').filter({ hasText: productName });
+  await helpers.click(product.locator('a.thumbnail.product-thumbnail').first());
 
-  const price = await getText(findByCss(page, 'span.current-price-value').first());
+  const price = await helpers.getText(helpers.findByCss(page, 'span.current-price-value').first());
   console.log('Product price:', price.trim());
 
   await page.pause();
